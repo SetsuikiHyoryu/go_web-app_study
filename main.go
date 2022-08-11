375bc2d316b3000035a4d0719f1df665322c4a72
SETSUIKI HYORYU <tayutai@outlook.jp>
2022-08-11 23:18:49 +0900

    feat: update

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "net/http"
 
 func main() {
-	http.ListenAndServe(":9491", http.FileServer(http.Dir("http_file-server_sample-file")))
+	http.ListenAndServe(":9491", http.FileServer(http.Dir("root")))
 }
