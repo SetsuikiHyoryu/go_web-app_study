ab524787aebc0763990c8ee1ab2dc72660e190bd
SETSUIKI HYORYU <tayutai@outlook.jp>
2022-09-13 07:58:44 +0900

    feature: update

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
@@ -11,60 +11,47 @@ func main() {
 		Addr: "localhost:9491",
 	}
 
-	// handlerForm := func(writer http.ResponseWriter, request *http.Request) {
-	// 	request.ParseForm()
+	http.HandleFunc("/write", func(write http.ResponseWriter, response *http.Request) {
+		_string := `
+			<html>
+			  <head><title>Go Web</title></head>
+			  <body><h1>Go Web</h1></body>
+			</html>
+		`
 
-	// 	// fmt.Fprintln(writer, request.Form) // map[string][]string
+		// Write 被调用时 header 中未设置 Content-Type 的话，数据的前 512 字节会被用来检测 Contetn-Type
+		// 如果没有显示调用 WriteHeader，那么会隐式地调用 WriteHeader(http.StatusOK)
+		write.Write([]byte(_string))
+	})
 
-	// 	// 当表单和 url 中含有相同的键名时，PostForm 可只选取表单中的值
-	// 	fmt.Fprintln(writer, request.PostForm) // map[string][]string
-	// }
+	http.HandleFunc("/write-header", func(write http.ResponseWriter, response *http.Request) {
+		write.WriteHeader(501)
+		fmt.Fprintln(write, "No such service, try next door")
+	})
 
-	// handlerMultipartForm := func(writer http.ResponseWriter, request *http.Request) {
-	// 	request.ParseMultipartForm(1024)
-	// 	fmt.Fprintln(writer, request.MultipartForm) // map[string][]string
-	// }
+	http.HandleFunc("/header", func(write http.ResponseWriter, request *http.Request) {
+		write.Header().Set("Location", "http://www.google.co.jp")
+		// 注意调用 WriteHeader 后无法再修改 header
+		write.WriteHeader(302)
+	})
 
-	// handlerValue := func(writer http.ResponseWriter, request *http.Request) {
-	// 	fmt.Fprintln(writer, request.FormValue("name"))
-	// 	fmt.Fprintln(writer, request.PostFormValue("name"))
-	// }
+	http.HandleFunc("/json", func(write http.ResponseWriter, request *http.Request) {
+		write.Header().Set("Content-Type", "application/json")
 
-	// handlerFile := func(writer http.ResponseWriter, request *http.Request) {
-	// 	// 传入的参数为字节数
-	// 	request.ParseMultipartForm(1024)
-	// 	// 因为文件可能是多个，所以用 0 取出第一个。返回的是指向一个 file header 的指针
-	// 	fileHeader := request.MultipartForm.File["filedata"][0]
-
-	// 	// 使用 Open 可以打开文件
-	// 	file, _error := fileHeader.Open()
-
-	// 	if _error == nil {
-	// 		// ioutil.ReadAll 可以把文件读取为 byte 切片
-	// 		data, _error := ioutil.ReadAll(file)
-
-	// 		if _error == nil {
-	// 			// 把文件内容写入 response
-	// 			fmt.Fprintln(writer, string(data))
-	// 		}
-	// 	}
-	// }
-
-	handlerFormFile := func(writer http.ResponseWriter, request *http.Request) {
-		// 更简单地获取第一个文件
-		file, _, _error := request.FormFile("filedata")
-
-		if _error == nil {
-			// ioutil.ReadAll 可以把文件读取为 byte 切片
-			data, _error := ioutil.ReadAll(file)
-
-			if _error == nil {
-				// 把文件内容写入 response
-				fmt.Fprintln(writer, string(data))
-			}
+		type Post struct {
+			User    string
+			Threads []string
 		}
-	}
 
-	http.HandleFunc("/api", handlerFormFile)
+		post := &Post{
+			User:    "hyoryu",
+			Threads: []string{"first", "seconde", "third"},
+		}
+
+		// json.Marshal 将数据转化为 json 编码
+		json, _ := json.Marshal(post)
+		write.Write(json)
+	})
+
 	server.ListenAndServe()
 }
