9a647f37cc3cef31afd344698da5c6178907179b
SETSUIKI HYORYU <tayutai@outlook.jp>
2022-09-13 09:07:03 +0900

    feature: update

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"text/template"
+	"time"
 )
 
 func main() {
@@ -11,47 +13,65 @@ func main() {
 		Addr: "localhost:9491",
 	}
 
-	http.HandleFunc("/write", func(write http.ResponseWriter, response *http.Request) {
-		_string := `
-			<html>
-			  <head><title>Go Web</title></head>
-			  <body><h1>Go Web</h1></body>
-			</html>
-		`
-
-		// Write 被调用时 header 中未设置 Content-Type 的话，数据的前 512 字节会被用来检测 Contetn-Type
-		// 如果没有显示调用 WriteHeader，那么会隐式地调用 WriteHeader(http.StatusOK)
-		write.Write([]byte(_string))
+	http.HandleFunc("/template", func(write http.ResponseWriter, request *http.Request) {
+		_template, _ := template.ParseFiles("template.html")
+		_template.Execute(write, "Hello World!")
 	})
 
-	http.HandleFunc("/write-header", func(write http.ResponseWriter, response *http.Request) {
-		write.WriteHeader(501)
-		fmt.Fprintln(write, "No such service, try next door")
+	http.HandleFunc("/parse-files", func(write http.ResponseWriter, request *http.Request) {
+		// 方式一
+		// _template, _ := template.ParseFiles("template.html")
+
+		// 方式二
+		_template := template.New("template")
+		_template, _ = _template.ParseFiles("template.html")
+
+		_template.Execute(write, "Hello World!")
 	})
 
-	http.HandleFunc("/header", func(write http.ResponseWriter, request *http.Request) {
-		write.Header().Set("Location", "http://www.google.co.jp")
-		// 注意调用 WriteHeader 后无法再修改 header
-		write.WriteHeader(302)
+	http.HandleFunc("/parse-glob", func(write http.ResponseWriter, request *http.Request) {
+		_template, _ := template.ParseGlob("*.html")
+		_template.Execute(write, "Hello World!")
 	})
 
-	http.HandleFunc("/json", func(write http.ResponseWriter, request *http.Request) {
-		write.Header().Set("Content-Type", "application/json")
+	http.HandleFunc("/execute-template", func(write http.ResponseWriter, request *http.Request) {
+		templates, _ := template.ParseFiles("template.html", "template02.html")
+		templates.ExecuteTemplate(write, "templatel02.html", "Hello World!")
+	})
 
-		type Post struct {
-			User    string
-			Threads []string
+	/** Full Example */
+	templates := loadTemplates()
+
+	http.HandleFunc("/", func(write http.ResponseWriter, request *http.Request) {
+		// 第一个字符是 /，所以将其切掉
+		fileName := request.URL.Path[1:]
+		_template := templates.Lookup(fileName)
+
+		if _template == nil {
+			write.WriteHeader(http.StatusNotFound)
+			return
 		}
 
-		post := &Post{
-			User:    "hyoryu",
-			Threads: []string{"first", "seconde", "third"},
-		}
+		_error := _template.Execute(write, "hello world.")
 
-		// json.Marshal 将数据转化为 json 编码
-		json, _ := json.Marshal(post)
-		write.Write(json)
+		if _error != nil {
+			log.Fatalln(_error.Error())
+		}
+	})
+
+	// Actions
+	http.HandleFunc("/actions", func(write http.ResponseWriter, request *http.Request) {
+		_template, _ := template.ParseFiles("actions.html")
+		rand.Seed(time.Now().Unix())
+		_template.Execute(write, rand.Intn(10) > 5)
 	})
 
 	server.ListenAndServe()
+}
+
+func loadTemplates() *template.Template {
+	result := template.New("templates")
+	_template, _error := result.ParseGlob("*.html")
+	template.Must(_template, _error)
+	return result
 }
